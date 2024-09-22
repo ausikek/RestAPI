0f83a46112f0d027e6f5d9e038a9a12810024d35
ausikek <orapacen2@outlook.com>
2024-09-22 00:53:40 -0300

    feat/Controllers: controllers written and routes updated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import userRouter from "./userRoutes";
 
 const router = Router();
 
+router.use("/users", userRouter);
 router.get("/", (_, res) => {
-  res.send("made by ausikek :>");
+  res.status(200).send("made by ausikek :>");
 });
 
 export default router;
